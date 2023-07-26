0da9566745883cb0110c4d355c6f5256459b1512
chahine-mahfoudh <chahine.mahfoudh@esprit.tn>
2023-07-26 16:34:26 +0100

    Dashbord

--- src/_metronic/partials/widgets/tables/TablesWidget14.tsx
+++ src/_metronic/partials/widgets/tables/TablesWidget14.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import {KTIcon, toAbsoluteUrl} from '../../../helpers'
-import gif from '../../../assets/images/7IsD.gif'
+import gif from '../../../assets/images/gif1.gif'
 type Props = {
   className: string
 }
@@ -17,7 +17,7 @@ const TablesWidget14: React.FC<Props> = ({className}) => {
       
        {/* Add your GIF here */}
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-    <img src={gif} alt='GIF' style={{ width: '750px' }}/>
+    <img src={gif} alt='GIF' style={{ width: '600px' }}/>
     </div>
 
       {/* end::Body */}
